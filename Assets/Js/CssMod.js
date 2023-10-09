2adf1819b3a90ba810055d0dfe2210957fdd5249
lttvrt <146584326+lttvrt@users.noreply.github.com>
2023-10-09 20:16:55 +0200

    Add files via upload

--- Assets/Js/CssMod.js
+++ Assets/Js/CssMod.js
@@ -1,16 +1,48 @@
+/*
+    @Auteur : DIONNE Clément
+    @Auteur : CELINI Alvin
+
+    @Date-de-création : 07/10/2023
+    @Date-de-modification : 09/10/2023
+    @Date-de-publication : -/-/2023
+
+    @description : Script JavaScript pour gérer le mode claire et sombre du site.
+*/
+// Attend que le contenu de la page soit chargé
 document.addEventListener('DOMContentLoaded', function () {
+    // Récupère les éléments HTML nécessaires
     const lightModeButton = document.getElementById('light-mode-button');
     const darkModeButton = document.getElementById('dark-mode-button');
     const lightStylesheet = document.getElementById('lightCSS');
     const darkStylesheet = document.getElementById('darkCSS');
-  
+
+    // Ajoute un écouteur d'événement pour le clic sur le bouton de mode clair
     lightModeButton.addEventListener('click', () => {
-      lightStylesheet.removeAttribute('disabled');
-      darkStylesheet.setAttribute('disabled', 'true');
+        // Active la feuille de style pour le mode clair et désactive celle pour le mode sombre
+        lightStylesheet.removeAttribute('disabled');
+        darkStylesheet.setAttribute('disabled', 'true');
+        // Stocke la préférence de l'utilisateur dans le stockage local
+        localStorage.setItem('preferredMode', 'light');
     });
-  
+
+    // Ajoute un écouteur d'événement pour le clic sur le bouton de mode sombre
     darkModeButton.addEventListener('click', () => {
-      darkStylesheet.removeAttribute('disabled');
-      lightStylesheet.setAttribute('disabled', 'true');
+        // Active la feuille de style pour le mode sombre et désactive celle pour le mode clair
+        darkStylesheet.removeAttribute('disabled');
+        lightStylesheet.setAttribute('disabled', 'true');
+        // Stocke la préférence de l'utilisateur dans le stockage local
+        localStorage.setItem('preferredMode', 'dark');
     });
-  });
+
+    // Vérifie s'il existe une préférence stockée et définit le thème en conséquence
+    const storedMode = localStorage.getItem('preferredMode');
+    if (storedMode === 'dark') {
+        // Active la feuille de style pour le mode sombre et désactive celle pour le mode clair
+        darkStylesheet.removeAttribute('disabled');
+        lightStylesheet.setAttribute('disabled', 'true');
+    } else {
+        // Active la feuille de style pour le mode clair et désactive celle pour le mode sombre (par défaut car dans le else)
+        lightStylesheet.removeAttribute('disabled');
+        darkStylesheet.setAttribute('disabled', 'true');
+    }
+    });
